821822660364d68a065c427b8b5c8eaf7dfbb956
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2020-08-25 18:13:57 -0400

    got snake moving

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const startButton = document.getElementById("start");
 const score = document.getElementById("score");
 let squares = [];
 let currentSnake = [2, 1, 0];
+let direction = 1;
+let width = 10;
 
 function createGrid() {
   for (let i = 0; i < 100; i++) {
@@ -19,24 +21,28 @@ currentSnake.forEach((index) => squares[index].classList.add("snake"));
 function move() {
   const tail = currentSnake.pop();
   squares[tail].classList.remove("snake");
-  currentSnake.unshift(currentSnake[0] + 1);
+  currentSnake.unshift(currentSnake[0] + direction);
   squares[currentSnake[0]].classList.add("snake");
 }
 
-let timerId = setInterval(move, 4000);
+let timerId = setInterval(move, 1000);
 
 function control(e) {
   switch (e.keyCode) {
     case 40:
       console.log("down pressed");
+      direction = +width;
       break;
     case 39:
+      direction = 1;
       console.log("right pressed");
       break;
     case 38:
+      direction = -width;
       console.log("up pressed");
       break;
     case 37:
+      direction = -1;
       console.log("left pressed");
       break;
   }
