4341fb6e76905492f3079d985351dda1362236d6
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2020-09-01 14:46:51 -0400

    made snake stop at barriers

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const score = document.getElementById("score");
 let squares = [];
 let currentSnake = [2, 1, 0];
 let direction = 1;
-let width = 10;
+const width = 10;
 
 function createGrid() {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < width * width; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     grid.appendChild(square);
@@ -19,12 +19,20 @@ createGrid();
 currentSnake.forEach((index) => squares[index].classList.add("snake"));
 
 function move() {
+  if (
+    (currentSnake[0] + width >= width * width && direction === width) ||
+    (currentSnake[0] % width === 9 && direction === 1) ||
+    (currentSnake[0] % width === 0 && direction === -1) ||
+    (currentSnake[0] - width < 0 && direction === -width) ||
+    squares[currentSnake[0] + direction].classList.contains("snake")
+  )
+    return clearInterval(timerId);
   const tail = currentSnake.pop();
   squares[tail].classList.remove("snake");
   currentSnake.unshift(currentSnake[0] + direction);
   squares[currentSnake[0]].classList.add("snake");
 }
-
+move();
 let timerId = setInterval(move, 1000);
 
 function control(e) {
